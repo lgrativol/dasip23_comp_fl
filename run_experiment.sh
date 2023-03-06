bf7d7def32f829c51d633fd703f94de512d5dcca
Lucas Grativol Ribeiro <67905648+lgrativol@users.noreply.github.com>
2023-03-06 16:30:48 +0100

    update script to resnet12

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,10 +1,10 @@
 
 declare -a PRUNING_RATE=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.90 0.95 0.99)
 declare -a CL_EPOCH=(1 10)
-declare -a ALPHA=(1)
+declare -a ALPHA=(100)
 
-CL_BS=16
-FMAPS=64
+CL_BS=20
+FMAPS=16
 CIFAR_10_CL_LR=0.01
 NUM_ROUNDS=100
 SAMP_CLIENT=0.4
@@ -16,18 +16,18 @@ for a in "${ALPHA[@]}"
     do
 
         ##Baseline
-        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model resnet8 --wbits 32 --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model resnet12 --wbits 32 --cl_epochs $cle --dataset $DATASET --alpha $a
 
         ##Binary
-        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model resnet8 --wbits 1 --bnn --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model resnet12 --wbits 1 --bnn --cl_epochs $cle --dataset $DATASET --alpha $a
 
             ##Quant
-        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model qresnet8 --wbits 4 --cl_epochs $cle --dataset $DATASET --alpha $a
-        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model qresnet8 --wbits 8 --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model qresnet12 --wbits 4 --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model qresnet12 --wbits 8 --cl_epochs $cle --dataset $DATASET --alpha $a
 
         for p in "${PRUNING_RATE[@]}"
         do
-            $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model resnet8 --cl_epochs $cle --dataset $DATASET --prune --prate $p --prune_srv --alpha $a
+            $PYTHON_CMD --cl_lr $CIFAR_10_CL_LR --model resnet12 --cl_epochs $cle --dataset $DATASET --prune --prate $p --prune_srv --alpha $a
         done
     done
 done
@@ -41,18 +41,18 @@ for a in "${ALPHA[@]}"
     do
 
         ##Baseline
-        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model resnet8 --wbits 32 --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model resnet12 --wbits 32 --cl_epochs $cle --dataset $DATASET --alpha $a
 
         ##Binary
-        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model resnet8 --wbits 1 --bnn --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model resnet12 --wbits 1 --bnn --cl_epochs $cle --dataset $DATASET --alpha $a
 
             ##Quant
-        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model qresnet8 --wbits 4 --cl_epochs $cle --dataset $DATASET --alpha $a
-        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model qresnet8 --wbits 8 --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model qresnet12 --wbits 4 --cl_epochs $cle --dataset $DATASET --alpha $a
+        $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model qresnet12 --wbits 8 --cl_epochs $cle --dataset $DATASET --alpha $a
 
         for p in "${PRUNING_RATE[@]}"
         do
-            $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model resnet8 --cl_epochs $cle --dataset $DATASET --prune --prate $p --prune_srv --alpha $a
+            $PYTHON_CMD --cl_lr $CIFAR_100_CL_LR --model resnet12 --cl_epochs $cle --dataset $DATASET --prune --prate $p --prune_srv --alpha $a
         done
     done
 done
